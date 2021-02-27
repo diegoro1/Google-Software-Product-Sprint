96814d963856ae5832e491cf201c357d75f111fc
Diego Rodrigues <di.rodrigues003@gmail.com>
2021-02-27 10:48:08 +0000

    Add Description

--- portfolio/src/main/webapp/src/components/About/index.js
+++ portfolio/src/main/webapp/src/components/About/index.js
@@ -13,7 +13,10 @@ export default function About() {
                         <Grid item xs={12} md={6}>
                             <div className="title">About me</div>
                             <div className="about-box">
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                                I am a Computer Engineering student at the University of Central Florida with a gigantic passion for Web Development. I am passionate about using both computer science and mathematics to develop designs with the best toolset possible as well as I am constantly looking to learn new technologies in order to better efficiency and delivery of projects while amplifying my leadership and personal skills.
+                            </div>
+                            <div className="about-box">
+                                My goal is to not only become the best developer I could possibly be but to better our society through ethics, diversity, and inclusion. My projects directly align with those principles, and I will continue working to develop a better tomorrow.
                             </div>
                         </Grid>
                         <Grid item xs={12} md={6}>
